32a713e591f2a309723425212f68be0e48485200
Andrey Kucherenko <kucherenko.andrey@gmail.com>
2015-02-27 14:13:10 +0000

    first release

--- index.js
+++ index.js
@@ -1,39 +1,61 @@
+var sensorLib = require('node-dht-sensor');
 var raspi = require('raspi-io');
 var five = require('johnny-five');
+
 var express = require('express');
 var serveStatic = require('serve-static');
 var app = express();
 var server = require('http').Server(app);
+
 var io = require('socket.io')(server);
 
+var servo, sensor;
 
 var board = new five.Board({
   io: new raspi()
 });
 
 board.on('ready', function() {
-
-  var servo = new five.Servo('P1-11');
-  servo.to(20);
-
-  this.repl.inject({
-    servo: servo
-  });
+  var servo = new five.Servo('P1-12');
+  servo.to(10);
 });
-
 
 app.use(serveStatic(__dirname, {
   'index': ['index.html'],
   'extensions': ['html', 'js']
 }))
-
 server.listen(8081);
 
 io.on('connection', function(socket) {
-  socket.emit('news', {
-    hello: 'world'
+
+  socket.on('start.servo', function() {
+    servo.to(110);
+    setTimeout(function() {
+      servo.to(10);
+    }, 1000);
   });
-  socket.on('my other event', function(data) {
-    console.log(data);
-  });
+
+  sensor = {
+    initialize: function() {
+      return sensorLib.initialize(22, 4);
+    },
+    read: function() {
+      var readout = sensorLib.read();
+      console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' +
+        'humidity: ' + readout.humidity.toFixed(2) + '%');
+
+      socket.emit('data.update', {
+        temp: readout.temperature.toFixed(2),
+        hum: readout.humidity.toFixed(2)
+      });
+      setTimeout(function() {
+        sensor.read();
+      }, 1000);
+    }
+  };
+  if (sensor.initialize()) {
+    sensor.read();
+  } else {
+    console.warn('Failed to initialize sensor');
+  }
 });
